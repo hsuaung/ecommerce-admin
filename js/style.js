985d0a74c5f7e0b22eea4f2fac706e11e4e9be4c
hsuaung <hsuaung633@gmail.com>
2024-01-19 14:01:29 +0630

    second

---
+++ js/style.js
@@ -0,0 +1,11 @@
+//side bar js
+
+// let btn = document.querySelector(".side-menu-bar");
+// let sidemenu = document.querySelector(".sidemenu");
+
+// btn.onclick = function () {
+//     sidemenu.classList.toggle("move");
+   
+
+// }
+
